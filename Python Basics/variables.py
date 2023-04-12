33d23b06c3f02d74148bf428ab17dbdf5915de72
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-04-13 02:39:11 +0530

    Variables
    •Variables in Python are used to store data or values.
    
    •A variable can have a name that can consist of letters, numbers, and underscore.
    
    •Variable names are case-sensitive, meaning "rent" and "Rent" are different variables.
    
    •A value can be assigned to a variable using the "=" operator.
    
    •The data type of a variable can change dynamically based on the value assigned.
    
    •Trying to assign a value to a reserved keyword, such as True or False, will raise an error.
    
    •Variable names cannot start with a number and should not be a Python built-in function name.
    
    •Mathematical operations can be performed on variables just like on numbers.

---
+++ Python Basics/variables.py
@@ -0,0 +1,15 @@
+rent = 1220
+gas = 202.5
+groceries = 305.6
+print(rent)
+1220
+total = rent+gas+groceries
+print(total)
+1728.1
+rent = 1400
+item1 = "rent"
+item2 = "gas"
+item3 = "groceries"
+print("Expense Items is : ", item1, item2, item3)
+# True = 4 //Error as True is a keyword
+# foo+bar = 4 //Error as variable name cannot start with an operator
